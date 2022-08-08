08d26986b7c592b6d19ef16def06816d4e8840d4
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-08-08 19:54:46 -0300

    Create router of success

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Checkout } from "./pages/Checkout";
+import { Success } from "./pages/Success";
 import { Home } from "./pages/Home";
 
 export function Router() {
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/checkout" element={<DefaultLayout />}>
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/checkout/success/:address" element={<Success />} />
       </Route>
     </Routes>
   );
